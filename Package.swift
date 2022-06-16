38a2db99f0cea103dd4b2fc7f17476a1222c016f
Joe Fabisevich <joe@fabisevi.ch>
2022-06-16 16:20:43 -0400

    Updating Boutique to point to a pre-release version of Bodega

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Boutique"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mergesort/Bodega.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/mergesort/Bodega.git", .upToNextMajor(from: "0.9.0"))
     ],
     targets: [
         .target(
