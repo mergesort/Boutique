7c7f16a9eada61816c745d1a434d40daadb73e8d
Joe Fabisevich <joe@fabisevi.ch>
2022-08-07 16:49:20 -0400

    Updating Performance Profiler to provide the newly required storage parameter

--- Performance Profiler/Sources/App/App.Store.swift
+++ Performance Profiler/Sources/App/App.Store.swift
@@ -1,5 +1,8 @@
 import Boutique
 
 extension Store where Item == RichNote {
-    static let notesStore = Store<RichNote>(cacheIdentifier: \.id)
+    static let notesStore = Store<RichNote>(
+        storage: SQLiteStorageEngine.default(appendingPath: "Notes"),
+        cacheIdentifier: \.id
+    )
 }
