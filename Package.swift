bf357967f46adeabae95c06e6125cb6cf8fdcb37
Joe Fabisevich <joe@fabisevi.ch>
2022-09-05 21:13:47 -0400

    Updating Bodega dependency to Bodega

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/mergesort/Bodega.git", exact: Version(2, 0, 0, prereleaseIdentifiers: ["rc-2"])),
+        .package(url: "https://github.com/mergesort/Bodega.git", exact: Version(2, 0, 0)),
         .package(url: "https://github.com/apple/swift-collections", exact: Version(1, 0, 2)),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
