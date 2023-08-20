2a9b4057a6cf591525cd0c96ac33050f456c01ea
Joe Fabisevich <joe@fabisevi.ch>
2023-08-20 18:22:37 -0400

    Adding a demonstration of how itemsHaveLoaded can be used for empty state views

--- Demo/Demo/App/ContentView.swift
+++ Demo/Demo/App/ContentView.swift
@@ -2,12 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var carouselFocusController = ScrollFocusController<String>()
+    @StateObject private var imagesController = ImagesController()
 
     var body: some View {
         VStack(spacing: 0.0) {
             FavoritesCarouselView()
                 .padding(.bottom, 8.0)
                 .environmentObject(carouselFocusController)
+                .environmentObject(imagesController)
 
             Divider()
 
