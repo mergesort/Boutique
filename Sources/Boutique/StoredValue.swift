f84c4897c35ddca0d8858a1351e2b04d761556cf
Joe Fabisevich <joe@fabisevi.ch>
2023-08-20 18:50:52 -0400

    StoredValue is now annotated with @MainActor

--- Sources/Boutique/StoredValue.swift
+++ Sources/Boutique/StoredValue.swift
@@ -38,6 +38,7 @@ import SwiftUI
 ///
 /// See: ``set(_:)`` and ``reset()`` docs for a more in depth explanation.
 @propertyWrapper
+@MainActor
 public struct StoredValue<Item: Codable & Equatable> {
     private let cancellableBox = CancellableBox()
     private let defaultValue: Item
@@ -92,10 +93,7 @@ public struct StoredValue<Item: Codable & Equatable> {
         let boxedValue = BoxedValue(value: value)
         if let data = try? JSONEncoder().encode(boxedValue) {
             self.userDefaults.set(data, forKey: self.key)
-
-            Task { @MainActor in
-                self.itemSubject.send(value)
-            }
+            self.itemSubject.send(value)
         }
     }
 
@@ -123,10 +121,7 @@ public struct StoredValue<Item: Codable & Equatable> {
         let boxedValue = BoxedValue(value: self.defaultValue)
         if let data = try? JSONEncoder().encode(boxedValue) {
             self.userDefaults.set(data, forKey: self.key)
-
-            Task { @MainActor in
-                self.itemSubject.send(self.defaultValue)
-            }
+            self.itemSubject.send(self.defaultValue)
         }
     }
 
