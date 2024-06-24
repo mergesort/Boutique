2ad2f9a2e3428efaf533d7fab4056132e0425d53
Joe Fabisevich <joe@fabisevi.ch>
2024-06-24 11:51:47 -0400

    Updating to Swift 5.10 and bumping platform targets to iOS 17/macOS 14

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Boutique",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v11),
+        .iOS(.v17),
+        .macOS(.v14),
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Boutique"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/mergesort/Bodega.git", exact: Version(2, 1, 0)),
+        .package(url: "https://github.com/mergesort/Bodega.git", exact: Version(2, 1, 3)),
         .package(url: "https://github.com/apple/swift-collections", from: Version(1, 0, 3)),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: Version(1, 0, 0)),
     ],
